5b06a8aa016a1c8d141436a42f28f05004163b5f
Austin Rohn <AustinRohn19@gmail.com>
2021-09-07 19:59:31 -0400

    updating the index.js

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const Meal = require('./Meal');
 const Weight = require('./Weight');
 
 
+<<<<<<< Updated upstream
 Meal.belongsToMany(User, {
     through: {
         model: Eat,
@@ -41,3 +42,34 @@ Fit.belongsTo(User, {
 
 
 module.exports = { User, Eat, Fit, Goals, Weight, Meal, };
+=======
+Goals.belongsToMany(User, {
+    through: {
+        model: Weight,
+        unique: false,
+    });
+
+User.belongsToMany(Goals, {
+    through: {
+        model: Weight,
+        unique: false,
+    });
+
+Meal.belongsToMany(User, {
+    through: {
+        model: Eat,
+        unique: false,
+    });
+
+User.belongsToMany(Meal, {
+    through: {
+        model: Eat,
+        unique: false,
+    });
+
+Fit.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Eat, Fit, Goals, Weight, Meal };
+>>>>>>> Stashed changes
