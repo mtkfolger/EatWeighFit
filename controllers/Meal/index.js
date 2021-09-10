2815ef0bae830811e819d1f9bfdf7eb47df86d27
Cialysse <marshall.cierra714@gmail.com>
2021-09-09 20:49:27 -0400

    updates to models

--- controllers/Meal/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const mealRoutes = require('../Meal');
-
-router.use('/', mealRoutes);
-
-
-module.exports = router
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-// const withAuth = require('../../utils/auth');
-// 
-router.get('/', async (req, res) => {
-    try {
-        const 
-    }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
